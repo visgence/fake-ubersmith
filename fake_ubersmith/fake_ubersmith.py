142a2c778a0e63e8468471044e9c0fd92741f7e4
Wajdi Al-Hawari <walhawari@iweb.com>
2017-02-15 14:34:45 -0500

    Fix incorrect usage of super that causes server to not start

--- fake_ubersmith/fake_ubersmith.py
+++ fake_ubersmith/fake_ubersmith.py
@@ -41,7 +41,7 @@ class FakeUbersmithError(object):
 class FakeUbersmith(FlaskServer):
 
     def __init__(self, port=9131, *args, **kwargs):
-        super(FakeUbersmith, self).__init__(self, "fake-ubersmith", port, *args, **kwargs)
+        super(FakeUbersmith, self).__init__("fake-ubersmith", port, *args, **kwargs)
 
         self.post_methods = {}
         self.service_plans = []
