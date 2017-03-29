40eaabe1d23bae34e2fa49fd983e1e8f4f75098b
Maxime Belanger <mbelanger@iweb.com>
2017-03-29 13:17:30 -0400

    Move unit tests in their own directory
    Will avoid mixing up integration tests and unit tests

--- tests/integration/test_ubersmith_client.py
+++ tests/integration/test_ubersmith_client.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from tests.integration_tests.base import Base
+from tests.integration.base import Base
 
 
 class TestUbersmithClient(Base):
